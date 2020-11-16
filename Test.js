269c0db7a7922b304e899f7de7255501dd52df6d
Daniel Teel <daniel.teel.2@us.af.mil>
2020-11-16 09:03:43 -0700

    Initial commit

---
+++ Test.js
@@ -0,0 +1,9 @@
+let {Point: Point, 
+  Line: Line,
+  ChartObject: ChartObject,
+  LinearChart: LinearChart,
+  ClampChart: ClampChart,
+  TrendChart: TrendChart,
+  PolyChart: PolyChart,
+  ChartTableEntry: ChartTableEntry,
+  ChartTable: ChartTable} = require(('./ChartObjects.js'));
